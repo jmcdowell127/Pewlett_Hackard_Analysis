a631124623f93c1e14dd06d28797101599e6dc3f
jmcdowell127 <85372441+jmcdowell127@users.noreply.github.com>
2021-08-08 20:19:35 -0500

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -10,7 +10,7 @@ SELECT e.emp_no,
 	   t.to_date
 INTO retirement_titles
 FROM employees AS e
-LEFT JOIN titles AS t
+JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
@@ -48,9 +48,9 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 			      t.title
 INTO mentorship_eligibility
 FROM employees AS e
-LEFT JOIN dept_emp AS de
+JOIN dept_emp AS de
 ON (e.emp_no = de.emp_no)
-LEFT JOIN titles AS t
+JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01' AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'))
 ORDER BY e.emp_no;
